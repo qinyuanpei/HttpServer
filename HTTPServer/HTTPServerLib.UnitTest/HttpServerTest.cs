023b5f8804633de8a6be5cb1b622c321cdc0e06a
qinyuanpei <qinyuanpei@163.com>
2017-08-09 23:32:04 +0800

    Add UnitTest Project

---
+++ HTTPServer/HTTPServerLib.UnitTest/HttpServerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Text;
+using System.IO;
+
+namespace HTTPServerLib.UnitTest
+{
+    [TestClass]
+    public class HttpServerTest
+    {
+        [TestMethod]
+        public void TestGet()
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://localhost:4050/");
+            var response = request.GetResponse();
+            Assert.AreEqual("text/html; charset=UTF-8", response.Headers["Content-Type"]);
+            Assert.AreEqual("ExampleServer", response.Headers["Server"]);
+        }
+
+        [TestMethod]
+        public void TestPost()
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://localhost:4050/");
+            request.Method = "POST";
+            var requestStream = request.GetRequestStream();
+            var data = Encoding.UTF8.GetBytes("a=10&b=15");
+            requestStream.Write(data,0,data.Length);
+            var response = request.GetResponse();
+            Assert.AreEqual("text/html; charset=UTF-8", response.Headers["Content-Type"]);
+            Assert.AreEqual("ExampleServer", response.Headers["Server"]);
+
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                var content = reader.ReadToEnd();
+                Assert.AreEqual("这是通过Post方式返回的数据:a=10;b=15", content);
+            }
+        }
+    }
+}
---
+++ HTTPServer/HTTPServerLib.UnitTest/HttpServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttpServerLib.UnitTest
+{
+    [TestClass]
+    public class HttpServiceTest
+    {
+        
+        [TestMethod]
+        public void TestGet()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestPost()
+        {
+
+        }
+    }
+}
