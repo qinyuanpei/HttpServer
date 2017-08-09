6a8dd49493256f0c77fd18c29007fa5325c2bc07
qinyuanpei <qinyuanpei@163.com>
2017-08-09 23:33:05 +0800

    Add HttpClient Project

---
+++ HTTPServer/HTTPClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://localhost:4050/");
+            request.Method = "POST";
+            var requestStream = request.GetRequestStream();
+            var data = Encoding.UTF8.GetBytes("a=10&b=15");
+            requestStream.Write(data, 0, data.Length);
+            var response = request.GetResponse();
+
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                var content = reader.ReadToEnd();
+            }
+        }
+    }
+}
